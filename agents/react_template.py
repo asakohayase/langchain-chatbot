aa5bf70e5fefc0428ca0702216248367f28d4037
Asako Hayase <chipmunk.kck@gmail.com>
2024-06-10 00:07:27 -0700

    feat: Add functionality to retrieve data from multiple sources

---
+++ agents/react_template.py
@@ -0,0 +1,21 @@
+from langchain_core.prompts import PromptTemplate
+
+def get_react_prompt_template():
+    return PromptTemplate.from_template("""Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+""")
